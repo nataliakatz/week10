9a2e6114741249b1e4226c4f14d206585b165012
nataliakatz <nathlienk@gmail.com>
2025-01-19 21:49:41 +0300

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    console.log('Я учу JavaScript!');
+}
+showMessage();
+
+const image1 = 'assets/images/kitten.jpeg';
+const image2 = 'assets/images/puppy.png';
+
+
+const prevButton = document.getElementById('prev-button');   // Кнопка "назад"
+const nextButton = document.getElementById('next-button');    // Кнопка "вперед"
+console.log(prevButton)
+
+
+function buttonClicked(nextButton) {
+    console.log(image2)
+}
